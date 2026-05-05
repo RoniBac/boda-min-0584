71d67b2f1cdb33cf399ea58eca403440c5459a72
Roni <ron.lopez.ru@gmail.com>
2026-05-04 20:22:45 -0600

    mensaje de cambios

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const API_URL = "/api"
+const API_URL = import.meta.env.DEV
+  ? "/api"
+  : "https://script.google.com/macros/s/AKfycbz-iiHNrt7RlrVQLO-Ip6nR9PT--R25KYy7CBgi4wN2YIpifltYZmbugNATBLX4OQeNjw/exec"
 
 // 🔍 Buscar invitado
 export const buscarInvitado = async (nombre) => {
@@ -6,17 +8,13 @@ export const buscarInvitado = async (nombre) => {
   return res.json()
 }
 
-// ✅ Confirmar invitado
 export const confirmarInvitado = async ({ fila, personas, asistencia }) => {
-  return fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      fila,
-      personas,
-      asistencia
-    })
+  const params = new URLSearchParams({
+    fila,
+    personas,
+    asistencia
   })
+
+  const res = await fetch(`${API_URL}?${params.toString()}`)
+  return res.json()
 }
